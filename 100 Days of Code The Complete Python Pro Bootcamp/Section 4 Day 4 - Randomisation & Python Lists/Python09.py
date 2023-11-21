9b684af1857f502cb1efd75b0e902d10d0ba05e9
Noor Mohammed Priom <noor105464@gmail.com>
2023-11-21 18:01:16 +0600

    P

--- 100 Days of Code The Complete Python Pro Bootcamp/Section 4 Day 4 - Randomisation & Python Lists/Python09.py
+++ 100 Days of Code The Complete Python Pro Bootcamp/Section 4 Day 4 - Randomisation & Python Lists/Python09.py
@@ -1,1 +1,4 @@
 # List element Positioning
+my_numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+print(f"{my_numbers[0]}, {my_numbers[1]}, {my_numbers[2]}, {my_numbers[9]}")
+print(f"{my_numbers[-1]}, {my_numbers[-2]}, {my_numbers[-3]}, {my_numbers[-10]}")
